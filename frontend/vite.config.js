2a29c4a4dc8fc97977dc71a20705c5749534558e
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-02-23 18:32:05 +0530

    Setup frontend for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
