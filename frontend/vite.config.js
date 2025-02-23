8029690fbe618427a4d52dc97ce44c05813dc6db
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-02-23 19:05:50 +0530

    Fixed axios issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,7 +28,24 @@ import react from '@vitejs/plugin-react'
 // });
 
 
+// export default defineConfig({
+//   build: {
+//     outDir: "dist",
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: process.env.VITE_API_URL,
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
+
+
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
